da462d57fe2dd1368b628ee925ba80318e137729
HF0 <HF0@users.noreply.github.com>
2020-12-10 18:30:43 +0100

    Upload problems 9 and 10

---
+++ src/AdventCode10/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AdventCode10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = File.ReadAllLines("input.txt")
+                .Select(e => int.Parse(e))
+                .OrderBy(e => e)
+                .ToList();
+            int solution1 = GetSolution1(numbers);
+            Console.WriteLine($"Solution 1: {solution1}");
+
+            long solution2 = GetSolution2(numbers);
+            Console.WriteLine($"Solution 1: {solution2}");
+        }
+
+        private static long GetSolution2(List<int> numbers)
+        {
+            Dictionary<int, long> calculatedConfigurations = new Dictionary<int, long>();
+            calculatedConfigurations.Add(numbers.Last(), 1);
+
+            Stack<int> configurationToCalculate = new Stack<int>();
+            configurationToCalculate.Push(numbers.First());
+            while (configurationToCalculate.Count() > 0)
+            {
+                var conf = configurationToCalculate.Peek();
+                if (calculatedConfigurations.ContainsKey(conf))
+                {
+                    configurationToCalculate.Pop();
+                    continue;
+                }
+
+                var compatible = GetCompatiblePlugs(conf, numbers);
+                var canCalculate = compatible.All(e => calculatedConfigurations.ContainsKey(e));
+                if (canCalculate)
+                {
+                    calculatedConfigurations.Add(conf, compatible.Select(e => calculatedConfigurations[e]).Sum());
+                    configurationToCalculate.Pop();
+                }
+                else
+                {
+                    AddAll(configurationToCalculate, compatible);
+                }
+            }
+            return calculatedConfigurations[0];
+        }
+
+        private static void AddAll(Stack<int> configurationToCalculate, IEnumerable<int> compatible)
+        {
+            foreach (var comp in compatible)
+            {
+                configurationToCalculate.Push(comp);
+            }
+        }
+
+        private static IEnumerable<int> GetCompatiblePlugs(int conf, List<int> numbers)
+        {
+            int index = numbers.BinarySearch(conf);
+            return numbers.Skip(index + 1).TakeWhile(e => e - conf <= 3);
+        }
+
+        private static int GetSolution1(List<int> numbers)
+        {
+            numbers.Insert(0, 0);
+            numbers.Add(numbers.Last() + 3);
+
+            var difference = numbers.Zip(numbers.Skip(1))
+                .Select(e => e.Second - e.First);
+
+            int dif1 = difference.Count(e => e == 1);
+            int dif3 = difference.Count(e => e == 3);
+            int solution = dif1 * dif3;
+            return solution;
+        }
+    }
+}
--- src/AdventCode9/Program.cs
+++ src/AdventCode9/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
@@ -8,10 +9,76 @@ namespace AdventCode9
     {
         static void Main(string[] args)
         {
-            var instructions = File.ReadAllLines("input.txt");
-            
-            int solution1a = 0;
+            var numbers = File.ReadAllLines("input.txt")
+                .Select(e => int.Parse(e));
+
+            int solution1a = GetSolution1(numbers);
             Console.WriteLine($"Solution 1: {solution1a}");
+
+            var listNumber = FindContiguousSum(solution1a, numbers);
+            int solution2a = listNumber.Max() + listNumber.Min();
+            Console.WriteLine($"Solution 2: {solution2a}");
+        }
+
+        private static int GetSolution1(IEnumerable<int> numbers)
+        {
+            var preamble = new Queue<int>(numbers.Take(25));
+            var remainingNumbers = numbers.Skip(25);
+            int solution1 = 0;
+            foreach (var n in remainingNumbers)
+            {
+                if (!CheckSum(n, preamble))
+                {
+                    solution1 = n;
+                    break;
+                }
+                preamble.Dequeue();
+                preamble.Enqueue(n);
+            }
+            return solution1;
+        }
+
+        private static IEnumerable<int> FindContiguousSum(int solution1a, IEnumerable<int> numbers)
+        {
+            int sum = 0;
+            Queue<int> result = new Queue<int>();
+            foreach(var n in numbers)
+            {
+                while (sum > solution1a && result.Count() > 0)
+                {
+                    int v = result.Dequeue();
+                    sum -= v;
+                }
+                if (sum == solution1a)
+                {
+                    break;
+                }
+                    
+                result.Enqueue(n);
+                sum += n;
+                if (sum == solution1a)
+                {
+                    break;
+                }
+            }
+            return result;
+        }
+
+        private static bool CheckSum(int sumValue, Queue<int> preamble)
+        {
+            ISet<int> neededNumber = new HashSet<int>();
+            foreach (var v in preamble)
+            {
+                if (neededNumber.Contains(v))
+                {
+                    return true;
+                }
+                else
+                {
+                    neededNumber.Add(sumValue - v);
+                }
+            }
+            return false;
         }
     }
 }
