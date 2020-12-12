bacae0e797d9ef989c629fb2f27bb688e5a73e79
HF0 <HF0@users.noreply.github.com>
2020-12-12 15:01:52 +0100

    Problems 11 and 12

---
+++ src/AdventCode11/Program.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace AdventCode11
+{
+    class Program
+    {
+        private const char EmptySeat = 'L';
+        private const char OccupiedSeat = '#';
+        private delegate char UpdateSeatFunction(char[][] grid, (int x, int y) seatPosition);
+        static void Main(string[] args)
+        {
+            var grid = File.ReadAllLines("input.txt")
+                .Select(e => e.ToCharArray())
+                .ToArray();
+
+            char[][] destination1 = CalculateEvolveSeats(grid, UpdateSeatProblem1);
+            int solution1 = CountOccupiedSeats(destination1);
+            Console.WriteLine($"Solution 1: {solution1}");
+
+            char[][] destination2 = CalculateEvolveSeats(grid, UpdateSeatProblem2);
+            int solution2 = CountOccupiedSeats(destination2);
+            Console.WriteLine($"Solution 2: {solution2}");
+        }
+
+        private static char[][] CalculateEvolveSeats(char[][] grid, UpdateSeatFunction updateSeat)
+        {
+            int width = grid[0].Length;
+            int height = grid.Length;
+
+            char[][] source = Clone(grid);
+            char[][] destination = CreateGrid(width, height);
+            while (UpdateGrid(source, destination, updateSeat))
+            {
+                char[][] tmp = source;
+                source = destination;
+                destination = tmp;
+            }
+            return destination;
+        }
+
+        private static char[][] Clone(char[][] grid) => grid.Select(s => s.ToArray()).ToArray();
+
+        private static char[][] CreateGrid(int width, int height)
+        {
+            var otherGrid = new char[height][];
+            for (int i = 0; i < height; i++)
+            {
+                otherGrid[i] = new char[width];
+            }
+            return otherGrid;
+        }
+
+        private static void Show(char[][] grid)
+        {
+            Console.WriteLine();
+            foreach (var row in grid)
+            {
+                Console.WriteLine(string.Join("", row));
+            }
+        }
+
+        private static int CountOccupiedSeats(char[][] grid) => grid.Select(e => e.Count(s => s.Equals(OccupiedSeat))).Sum();
+
+        private static bool UpdateGrid(char[][] grid, char[][] gridOutput, UpdateSeatFunction updateSeatFunction)
+        {
+            int width = grid[0].Length;
+            int height = grid.Length;
+            bool changed = false;
+            for (int x = 0; x < height; x++)
+            {
+                for (int y = 0; y < width; y++)
+                {
+                    char currentSeatStatus = grid[x][y];
+                    gridOutput[x][y] = updateSeatFunction(grid, (x, y));
+                    changed = changed || !gridOutput[x][y].Equals(currentSeatStatus);
+                }
+            }
+            return changed;
+        }
+
+        private static char UpdateSeatProblem1(char[][] grid, (int x, int y) position)
+        {
+            char place = grid[position.x][position.y];
+            return place switch
+            {
+                EmptySeat => AdjancentOccupiedSeats(position, grid) == 0 ? OccupiedSeat : place,
+                OccupiedSeat => AdjancentOccupiedSeats(position, grid) >= 4 ? EmptySeat : place,
+                _ => grid[position.x][position.y],
+            };
+        }
+
+        private static char UpdateSeatProblem2(char[][] grid, (int x, int y) position)
+        {
+            char place = grid[position.x][position.y];
+            return place switch
+            {
+                EmptySeat => EyeOccupiedSeat(position, grid) == 0 ? OccupiedSeat : place,
+                OccupiedSeat => EyeOccupiedSeat(position, grid) >= 5 ? EmptySeat : place,
+                _ => grid[position.x][position.y],
+            };
+        }
+
+        private static int AdjancentOccupiedSeats((int x, int y) position, char[][] grid)
+        {
+            int count = 0;
+            for (int x = position.x - 1; x <= position.x + 1; x++)
+            {
+                for (int y = position.y - 1; y <= position.y + 1; y++)
+                {
+                    if (ValidPosition((x, y), grid) &&
+                        (!position.x.Equals(x) || !position.y.Equals(y)) &&
+                        grid[x][y].Equals(OccupiedSeat))
+                    {
+                        count++;
+                    }
+                }
+            }
+            return count;
+        }
+
+        private static int EyeOccupiedSeat((int x, int y) position, char[][] grid)
+        {
+            int count = 0;
+            for (int offsetX = -1; offsetX <= 1; offsetX++)
+            {
+                for (int offsetY = -1; offsetY <= 1; offsetY++)
+                {
+                    if ((offsetX != 0 || offsetY != 0) &&
+                        CanSeeOccupiedSeat((offsetX, offsetY), position, grid))
+                    {
+                        count++;
+                    }
+                }
+            }
+            return count;
+        }
+
+        private static bool CanSeeOccupiedSeat((int x, int y) offset, (int x, int y) position, char[][] grid)
+        {
+            position.x += offset.x;
+            position.y += offset.y;
+            while (ValidPosition(position, grid))
+            {
+                char seat = grid[position.x][position.y];
+                if (seat.Equals(OccupiedSeat))
+                {
+                    return true;
+                }
+                if (seat.Equals(EmptySeat))
+                {
+                    return false;
+                }
+                position.x += offset.x;
+                position.y += offset.y;
+            }
+            return false;
+        }
+
+        private static bool ValidPosition((int x, int y) position, char[][] grid)
+        {
+            int width = grid[0].Length;
+            int height = grid.Length;
+            return position.x >= 0 && position.x < height && position.y >= 0 && position.y < width;
+        }
+    }
+}
---
+++ src/AdventCode12/Program.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AdventCode12
+{
+    class Program
+    {
+        private delegate void ProcessInstructionDelegate((char action, int value) instruction, ref (int x, int y) pos, ref (int x, int y) direction);
+        private static readonly (int, int) InitialPosition = (0, 0);
+        static void Main(string[] args)
+        {
+            var instructions = File.ReadAllLines("input.txt")
+                .Select(e => ParseInstruction(e));
+
+            // n/s e/w
+            (int, int) waypoint = (0, 1);
+            (int, int) position = InitialPosition;
+            int solution1 = GetSolution(instructions, ref waypoint, ref position, ProcessInstructionProblem1);
+            Console.WriteLine($"Solution 1: {solution1}");
+
+            (int, int) waypoint2 = (1, 10);
+            (int, int) position2 = InitialPosition;
+            int solution2 = GetSolution(instructions, ref waypoint2, ref position2, ProcessInstructionProblem2);
+            Console.WriteLine($"Solution 2: {solution2}");
+        }
+
+        private static int GetSolution(IEnumerable<(char, int)> instructions, ref (int, int) waypoint, ref (int, int) position,
+            ProcessInstructionDelegate processInstruction)
+        {
+            foreach (var instruction in instructions)
+            {
+                processInstruction(instruction, ref position, ref waypoint);
+            }
+            return ManhattanDistance(position, InitialPosition);
+        }
+
+
+        private static void ProcessInstructionProblem1((char action, int value) instruction, ref (int x, int y) pos, ref (int x, int y) direction)
+        {
+            switch (instruction.action)
+            {
+                case 'N':
+                    pos = AdvancePosition(instruction.value, pos, (1, 0));
+                    break;
+                case 'S':
+                    pos = AdvancePosition(instruction.value, pos, (-1, 0));
+                    break;
+                case 'E':
+                    pos = AdvancePosition(instruction.value, pos, (0, 1));
+                    break;
+                case 'W':
+                    pos = AdvancePosition(instruction.value, pos, (0, -1));
+                    break;
+                case 'L':
+                    direction = ChangeDirection(direction, -1 * instruction.value);
+                    break;
+                case 'R':
+                    direction = ChangeDirection(direction, instruction.value);
+                    break;
+                case 'F':
+                    pos = AdvancePosition(instruction.value, pos, direction);
+                    break;
+                default:
+                    throw new ArgumentException();
+            };
+        }
+
+        private static void ProcessInstructionProblem2((char action, int value) instruction, ref (int x, int y) pos, ref (int x, int y) waypoint)
+        {
+            switch (instruction.action)
+            {
+                case 'N':
+                    waypoint = AdvancePosition(instruction.value, waypoint, (1, 0));
+                    break;
+                case 'S':
+                    waypoint = AdvancePosition(instruction.value, waypoint, (-1, 0));
+                    break;
+                case 'E':
+                    waypoint = AdvancePosition(instruction.value, waypoint, (0, 1));
+                    break;
+                case 'W':
+                    waypoint = AdvancePosition(instruction.value, waypoint, (0, -1));
+                    break;
+                case 'L':
+                    waypoint = ChangeDirection(waypoint, -1 * instruction.value);
+                    break;
+                case 'R':
+                    waypoint = ChangeDirection(waypoint, instruction.value);
+                    break;
+                case 'F':
+                    pos = AdvancePosition(instruction.value, pos, waypoint);
+                    break;
+                default:
+                    throw new ArgumentException();
+            };
+        }
+
+        private static (int x, int y) AdvancePosition(int distance, (int x, int y) pos, (int x, int y) waypoing)
+        {
+            pos.x += waypoing.x * distance;
+            pos.y += waypoing.y * distance;
+            return pos;
+        }
+
+        private static (int x, int y) ChangeDirection((int x, int y) direction, int value)
+        {
+            if (value % 90 != 0)
+            {
+                throw new ArgumentException();
+            }
+            value /= 90;
+            value %= 4;
+            if (value == 0)
+            {
+                return direction;
+            }
+            (int x, int y) newDirection;
+            switch (value)
+            {
+                case 1:
+                case -3:
+                    newDirection = (direction.y, direction.x);
+                    if (direction.y != 0)
+                    {
+                        newDirection.x *= -1;
+                    }
+                    break;
+                case 3:
+                case -1:
+                    newDirection = (direction.y, direction.x);
+                    if (direction.x != 0)
+                    {
+                        newDirection.y *= -1;
+                    }
+                    break;
+                case 2:
+                case -2:
+                    newDirection = (direction.x * -1, direction.y * -1);
+                    break;
+                default:
+                    throw new ArgumentException();
+            }
+            return newDirection;
+        }
+
+        private static (char, int) ParseInstruction(string e) => (e[0], int.Parse(e[1..]));
+        private static int ManhattanDistance((int x, int y) p1, (int x, int y) p2) => Math.Abs(p1.x - p2.x) + Math.Abs(p1.y - p2.y);
+
+    }
+}
